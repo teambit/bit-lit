215b1f50f36146d34a91ac708244ea7281808339
Gilad Shoham <shoham.gilad@gmail.com>
2021-08-19 23:37:59 +0300

    update jest config - change transform function, change transform ignore patterns

--- web-components/lit/jest/jest.config.js
+++ web-components/lit/jest/jest.config.js
@@ -1,15 +1,28 @@
-const tsConfigFile = require.resolve("./tsconfig.json");
+const reactJestConfig = require('@teambit/react/jest/jest.config');
+const packagesToTransform = [
+  "lit",
+  "@lit",
+  "testing-library__dom",
+  "@open-wc",
+  "lit-html",
+  "lit-element",
+  "pure-lit",
+  "lit-element-state-decoupler",
+];
+
+const negativeLookahead = packagesToTransform.reduce((acc, curr) => {
+  const yarnPattern = curr;
+  const pnpmPattern = `.pnpm/registry.npmjs.org/${curr}.*`;
+  if (acc) {
+    return `${acc}|${yarnPattern}|${pnpmPattern}`;
+  }
+  return `${yarnPattern}|${pnpmPattern}`;
+}, "");
+const transformIgnorePatterns = `node_modules/(?!(${negativeLookahead})/)`;
 
 module.exports = {
-  preset: "ts-jest/presets/js-with-ts",
-  globals: {
-    "ts-jest": {
-      tsconfig: tsConfigFile,
-    },
-  },
-  transformIgnorePatterns: [
-    "node_modules/(?!(testing-library__dom|@open-wc|lit-html|lit-element|pure-lit|lit|@lit|lit-element-state-decoupler)/)",
-  ],
+  transform: reactJestConfig.transform,
+  transformIgnorePatterns: [transformIgnorePatterns],
   setupFilesAfterEnv: [require.resolve("./setupTests.js")],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
