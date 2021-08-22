ce014e75d0f814e29ce1468107f93d829d50c84c
Itay Mendelawy <itaymendel@users.noreply.github.com>
2021-08-22 21:27:35 +0300

    Update readme-file.ts

--- web-components/lit/templates/lit-workspace/files/readme-file.ts
+++ web-components/lit/templates/lit-workspace/files/readme-file.ts
@@ -1,26 +1,56 @@
 export function readme() {
-  return `# Welcome to your Bit with Lit Workspace
+  return `# Bit workspace for Lit components
 
-## What's included
+This workspace contains a set of independently versioned and published [Lit](https://lit.dev/) components.
 
-- **workspace.jsonc**
+## Setup workspace
 
-This is the main configuration file of your bit workspace. Here you can modify the workspace name and icon as well as default directory and scope. It is where dependencies are found when you install anything. It is also where you register aspects, bit extensions as well as apply the environments for your components. This workspace has been setup so that all components use the React env. However you can create other components and apply other envs to them such as node, html, angular and aspect envs.
-This template come with the official Lit aspect pre-defined with the latest official version.
+1. [Install Bit on your system](https://harmony-docs.bit.dev/getting-started/installing-bit)).
+1. Clone this repository.
+1. Run \`bit install\`.
 
-- **.bitmap**
+Start the component dev server:
 
-This is an auto-generated file and includes the mapping of your components. There is one component included here. In order to remove this component you can run the following command.
+\`\`\`sh
+bit start
+\`\`\`
 
-- **.gitignore**
+Add dependencies:
 
-Ignoring any files from version control
+\`\`\`sh
+bit install my-dependency
+\`\`\`
 
-## Adding new components
-run \`bit create lit my-lit-component\`
+Create a new component
 
-## Run this workspace
-Make sure to first run \`bit compile\` then \`bit install\`.
-Then you can run \`bit start\` and open [localhost:3000](http://localhost:3000)
+\`\`\`sh
+bit create lit my-component
+\`\`\`
+
+## Component features
+
+The following files can be added to each component to take advantage of further features Bit provides.
+
+- \`my-component.docs.md/x\` - Add documentation for your component.
+- \`my-component.specs.js\` - Add test to your component.
+- \`my-component.composition.js\` - Mount your component in a demo application, or multiple applications/contexts, to see it rendered.
+
+## Version and publish
+
+To publish components run:
+
+\`\`\`sh
+bit tag -a -m 'tag message here' && bit export
+\`\`\`
+
+Note - It's recommended to publish components as part of your CI process. Learn more [here](https://harmony-docs.bit.dev/reference/ci-cd#version-and-export-components-on-ci).
+
+## Maintainer docs
+
+There are two files to be away when working with Bit:
+
+- **workspace.jsonc** This is the main configuration file of your bit workspace. Here you can modify the workspace name and icon as well as default directory and scope. It is where dependencies are found when you install anything. It is also where you register aspects, Bit extensions as well as apply configurations for your components. This workspace has been setup so that all components use the Lit env. However you can create other components and apply other envs to them such as node, html, angular others.
+- **.bitmap** This is an auto-generated file and includes the mapping of your components.
+
 `;
 }
