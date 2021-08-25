d7199509873afd68ff7718975fc91dd5969ea03e
Gilad Shoham <shoham.gilad@gmail.com>
2021-08-25 16:45:28 +0300

    update jest regex

--- web-components/lit/jest/jest.config.js
+++ web-components/lit/jest/jest.config.js
@@ -13,8 +13,12 @@ const packagesToTransform = [
 const negativeLookahead = packagesToTransform.reduce((acc, curr) => {
   const yarnPattern = curr;
   const pnpmPattern = `.pnpm/registry.npmjs.org/${curr}.*`;
+  // The new version of pnpm is not adding the registry as part of the path
+  // so adding this as well to support it
+  const newPnpmPattern = `.pnpm/${curr}.*`;
+
   if (acc) {
-    return `${acc}|${yarnPattern}|${pnpmPattern}`;
+    return `${acc}|${yarnPattern}|${pnpmPattern}|${newPnpmPattern}`;
   }
   return `${yarnPattern}|${pnpmPattern}`;
 }, "");
