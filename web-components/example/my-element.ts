18b77bab0e01b71efb0046a7a3186f3a32ea1ad2
Gilad Shoham <shoham.gilad@gmail.com>
2021-08-31 16:49:42 +0300

    change the example to use styles from scss file

--- web-components/example/my-element.ts
+++ web-components/example/my-element.ts
@@ -1,13 +1,12 @@
 import {html, css, LitElement} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
+import styles from "./my-component.scss";
 
 @customElement("simple-greeting")
 export class SimpleGreeting extends LitElement {
-  static styles = css`
-    p {
-      color: blue;
-    }
-  `;
+  static get styles() {
+    return [styles];
+  }
 
   @property()
   name = "Somebody";
