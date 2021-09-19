19ddfe3e872f82a3d9c9bb547e677802c227d4e3
bengil <benjyg@gmail.com>
2021-09-19 16:40:50 +0300

    use generateExcludePattern instead of local logic

--- web-components/lit/jest/jest.config.js
+++ web-components/lit/jest/jest.config.js
@@ -1,3 +1,5 @@
+import { generateNodeModulesPattern } from '@teambit/dependencies.modules.packages-excluder';
+
 const reactJestConfig = require('@teambit/react/jest/jest.config');
 const packagesToTransform = [
   "lit",
@@ -10,23 +12,9 @@ const packagesToTransform = [
   "lit-element-state-decoupler",
 ];
 
-const negativeLookahead = packagesToTransform.reduce((acc, curr) => {
-  const yarnPattern = curr;
-  const pnpmPattern = `.pnpm/registry.npmjs.org/${curr}.*`;
-  // The new version of pnpm is not adding the registry as part of the path
-  // so adding this as well to support it
-  const newPnpmPattern = `.pnpm/${curr}.*`;
-
-  if (acc) {
-    return `${acc}|${yarnPattern}|${pnpmPattern}|${newPnpmPattern}`;
-  }
-  return `${yarnPattern}|${pnpmPattern}|${newPnpmPattern}`;
-}, "");
-const transformIgnorePatterns = `node_modules/(?!(${negativeLookahead})/)`;
-
 module.exports = {
   transform: reactJestConfig.transform,
-  transformIgnorePatterns: [transformIgnorePatterns],
+  transformIgnorePatterns: [generateNodeModulesPattern({ packages: packagesToTransform })],
   setupFilesAfterEnv: [require.resolve("./setupTests.js")],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
