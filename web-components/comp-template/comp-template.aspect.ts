b637e6e89185c122a5443f14f52a86e4cc7d260f
bengil <benjyg@gmail.com>
2022-03-09 20:53:02 +0200

    remove unused code

--- web-components/comp-template/comp-template.aspect.ts
+++
@@ -1,6 +0,0 @@
-import { Aspect } from '@teambit/harmony';
-
-export const CompTemplateAspect = Aspect.create({
-  id: 'teambit.web-components/comp-template',
-});
-  
--- web-components/comp-template/index.ts
+++
@@ -1,5 +0,0 @@
-import { CompTemplateAspect } from './comp-template.aspect';
-
-export type { CompTemplateMain } from './comp-template.main.runtime';
-export default CompTemplateAspect;
-export { CompTemplateAspect };
